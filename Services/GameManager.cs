886d6efaf2e305ef7dc3d18243b6ac6449e4ae3b
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-10 12:48:00 +0100

    Added game services

---
+++ Services/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using PaperRockScissors_MauiGame.Models;
+using PaperRockScissors_MauiGame.Services.Interfaces;
+
+namespace PaperRockScissors_MauiGame.Services
+{
+    public class GameManager : IGameManager
+    {
+        public string CheckWhoWin(IGameChoice player, IGameChoice oponent)
+        {
+            if (player.Name == oponent.Name)
+                return "Draw";
+            else if (player.Strong == oponent.Name)
+                return "Win";
+            else
+                return "Loose";
+        }
+
+        public IGameChoice SetChoice(string choice)
+        {
+            return choice switch
+            {
+                "Paper" => new Paper(),
+                "Rock" => new Rock(),
+                "Scissors" => new Scissors(),
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        public IGameChoice RandChoice()
+        {
+            string[] choices = ["Paper", "Rock", "Scissors"];
+            Random rnd = new();
+
+            rnd.Shuffle(choices);
+
+            int randomChoice = rnd.Next(choices.Length);
+
+            return SetChoice(choices[randomChoice]);
+        }
+    }
+}
