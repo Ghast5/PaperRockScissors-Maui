c106434662778f7634c933411f661b715f71c301
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-10 13:02:33 +0100

    Registered routes

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace PaperRockScissors_MauiGame
+﻿using PaperRockScissors_MauiGame.Views;
+
+namespace PaperRockScissors_MauiGame
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(ResultView), typeof(ResultView));
+            Routing.RegisterRoute(nameof(GameView), typeof(GameView));
+            Routing.RegisterRoute("MainView", typeof(MainPage));
         }
     }
 }
