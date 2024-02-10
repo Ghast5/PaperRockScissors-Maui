8bf1e5b1f71a37a8b94f881505f0b8c5635edac9
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-10 12:59:29 +0100

    Added interface for ResultViewModel

---
+++ ViewModels/ResultViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PaperRockScissors_MauiGame.Views;
+
+namespace PaperRockScissors_MauiGame.ViewModels
+{
+    [QueryProperty(nameof(Score), "score")]
+    public partial class ResultViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string score;
+
+        [RelayCommand]
+        public async Task PlayAgain() => await Shell.Current.GoToAsync(nameof(GameView));
+
+        [RelayCommand]
+        public async Task Exit() => await Shell.Current.GoToAsync("MainView");
+    }
+}
