72f9d4961f748cc7a843f0a548ff50e60315f844
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-10 12:56:01 +0100

    Added ViewModel for GameView

---
+++ ViewModels/GameViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PaperRockScissors_MauiGame.Views;
+using PaperRockScissors_MauiGame.Services.Interfaces;
+
+namespace PaperRockScissors_MauiGame.ViewModels
+{
+    public partial class GameViewModel : ObservableObject
+    {
+        private readonly IGameManager gameManager;
+
+        public GameViewModel(IGameManager gameManager)
+        {
+            this.gameManager = gameManager;
+        }
+
+        [RelayCommand]
+        public async Task Choose(string choice)
+        {
+            IGameChoice playerChoice = gameManager.SetChoice(choice);
+            IGameChoice oponentChoice = gameManager.RandChoice();
+
+            string score = gameManager.CheckWhoWin(playerChoice, oponentChoice);
+
+            await ResultRedirect(score);
+        }
+
+        private async Task ResultRedirect(string score) => await Shell.Current.GoToAsync($"{nameof(ResultView)}?score={score}");
+
+        [RelayCommand]
+        public async Task Cancel() => await Shell.Current.Navigation.PopModalAsync();
+    }
+}
