af5897a2a33b70e1adc562b75a40ae4e5c1377dd
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-10 12:44:42 +0100

    Added interfaces

---
+++ Services/Interfaces/IGameChoice.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PaperRockScissors_MauiGame.Services.Interfaces
+{
+    public interface IGameChoice
+    {
+        string Name { get; }
+        string Strong { get; }
+        string Weakness { get; }
+    }
+}
