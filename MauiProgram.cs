81e104f771c71cc4e60739cd7c1a2b2c7c9ef1ae
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-10 13:38:10 +0100

    Added dependency injection

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using PaperRockScissors_MauiGame.Views;
+using PaperRockScissors_MauiGame.ViewModels;
+using PaperRockScissors_MauiGame.Services;
+using PaperRockScissors_MauiGame.Services.Interfaces;
 
 namespace PaperRockScissors_MauiGame
 {
@@ -15,8 +19,14 @@ namespace PaperRockScissors_MauiGame
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddTransient<GameView>();
+            builder.Services.AddTransient<GameViewModel>();
+            builder.Services.AddTransient<ResultView>();
+            builder.Services.AddTransient<ResultViewModel>();
+            builder.Services.AddTransient<IGameManager, GameManager>();
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
