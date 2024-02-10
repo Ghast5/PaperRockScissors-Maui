a060a1d870c109f1e2bf85b06c1d1cfd7a10ade2
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-10 12:52:02 +0100

    Added ViewModel for MainPage

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PaperRockScissors_MauiGame.Views;
+
+namespace PaperRockScissors_MauiGame.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [RelayCommand]
+        public async Task PlayRedirect() => await Shell.Current.GoToAsync(nameof(GameView));
+    }
+}
