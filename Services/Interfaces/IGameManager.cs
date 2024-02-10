ad38d598ab9bba2e18b31d7c5e0e060a5ac2d1bf
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-10 12:46:14 +0100

    Added intefaces for game manager

---
+++ Services/Interfaces/IGameManager.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PaperRockScissors_MauiGame.Services.Interfaces
+{
+    public interface IGameManager
+    {
+        string CheckWhoWin(IGameChoice player, IGameChoice oponent);
+        IGameChoice SetChoice(string choice);
+        IGameChoice RandChoice();
+    }
+}
