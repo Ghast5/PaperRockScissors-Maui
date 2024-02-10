7d13a775413449e9b3d00d66281689c9c8464d7a
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-10 12:52:15 +0100

    Added content to MainPage

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace PaperRockScissors_MauiGame.Views
+﻿using PaperRockScissors_MauiGame.ViewModels;
+
+namespace PaperRockScissors_MauiGame.Views
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
+            BindingContext = new MainViewModel();
             InitializeComponent();
         }
     }
