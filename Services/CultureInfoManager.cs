dc1ab887e12ec137c83f839c18935020dadc60bc
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-13 23:22:14 +0100

    Added manager to support languages

---
+++ Services/CultureInfoManager.cs
@@ -0,0 +1,20 @@
+﻿using PaperRockScissors_MauiGame.Services.Enums;
+using PaperRockScissors_MauiGame.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperRockScissors_MauiGame.Services
+{
+    public class CultureInfoManager : ICultureInfoManager
+    {
+        public string GetCultureInfo() => Preferences.Get("UserLanguage", CultureInfo.GetCultureInfo(CultureInfo.CurrentUICulture.Name).ToString());
+        public void SetCultureInfo(Languages culture)
+        {
+            Preferences.Set("UserLanguage", culture.ToString());
+        }
+    }
+}
