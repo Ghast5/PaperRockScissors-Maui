b91cb780c51fe05dfbe315b042aa1e025d88c64b
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-19 17:01:56 +0100

    Added score storage
    Resolve #21

---
+++ Services/Data/ScoreStorage.cs
@@ -0,0 +1,12 @@
+﻿using PaperRockScissors_MauiGame.Models;
+
+namespace PaperRockScissors_MauiGame.Services.Data
+{
+    public class ScoreStorage
+    {
+        private List<ScoreEntry> scoreEntries = [];
+
+        public void AddScore(ScoreEntry entry) =>  scoreEntries.Add(entry);
+        public List<ScoreEntry> GetScoreList() => scoreEntries;
+    }
+}
