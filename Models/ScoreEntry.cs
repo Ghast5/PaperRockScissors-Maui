17ef48009edebd3555721baef28f3344c2891bdc
Damian <48653060+Ghast5@users.noreply.github.com>
2024-02-19 17:00:38 +0100

    Created model for score entry
    Resolve #21

---
+++ Models/ScoreEntry.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PaperRockScissors_MauiGame.Models
+{
+    public class ScoreEntry
+    {
+        public int Id { get; set; }
+        public string PlayerChoice { get; set; }
+        public string OpponentChoice { get; set; }
+        public string Score { get; set; }
+    }
+}
